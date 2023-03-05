66fb63257ff361595f57128b3792c345bc34b576
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-03-05 16:05:32 +0530

    app styled & structured

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
-
+import Typing from "./Asset/img/typing.gif";
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="app">
+      <div className="app-container">
+        <h1 className="app-title">how fast do you type?</h1>
+        <img src={Typing} alt="typing" />
+        <textarea className="app-input" />
+        <h2 className="app-time">time remaining: 5</h2>
+        <button className="app-button">start</button>
+        <h3 className="app-count">word count: 10</h3>
+      </div>
+    </div>
+  );
 }
 
 export default App;
